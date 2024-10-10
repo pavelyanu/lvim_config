931f2ec7eab4b25a827d0095056442562c0584bf
Pavel Yanushonak <pyanushonak@gmail.com>
2024-10-10 17:49:27 +0200

    Initial commit

---
+++ config.lua
@@ -0,0 +1,20 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+    end
+  },
+}
+lvim.colorscheme = "gruvbox-material"
+lvim.keys.normal_mode["<C-t>"] = ":NvimTreeToggle<CR>"
+lvim.keys.normal_mode["<C-i>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<C-S-I>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<C-w>"] = ":BufferLineCloseRight<CR>"
