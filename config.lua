d9fb71a744665a27a337d3b995facf9d31090e77
pavelyanu <pyanushonak@gmail.com>
2024-10-10 19:24:26 +0200

    tabwith

--- config.lua
+++ config.lua
@@ -13,8 +13,15 @@ lvim.plugins = {
     end
   },
 }
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 lvim.colorscheme = "gruvbox-material"
 lvim.keys.normal_mode["<C-t>"] = ":NvimTreeToggle<CR>"
 lvim.keys.normal_mode["<C-i>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<C-S-I>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<C-w>"] = ":BufferLineCloseRight<CR>"
+
+
